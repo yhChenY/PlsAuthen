95e645fefd1973151d89b829b605be7f80caa57b
qwq-y <2090078073@qq.com>
2023-01-05 18:34:01 +0800

    修复了CTAP2认证过程中，错误输入pin时的空指针和数组越界错误。

--- src/Client.java
+++ src/Client.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
 
 
 public class Client {
-    
+
     int clientId;
-    
-    
+
+
     static String requestSetup(String pkAStr) {
         try {
             // 获取pin
             String pin = generatePin(6);
             // 返回公钥、加密（pin、对称密钥）
             return generateRst(pin, pkAStr);
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-    
+
     static String requestBind(String pkAStr) {
         // 获取用户输入的pin
         Scanner scanner = new Scanner(System.in);
@@ -31,11 +31,13 @@ public class Client {
         scanner.close();
         // 根据pin生成pinH
         String pinH = generatePinH(pin);
-        
+
         // 返回公钥、加密（pinH、对称密钥）
-        return generateRst(pinH, pkAStr);
+        String rst = generateRst(pinH, pkAStr);
+        System.out.println("client return: " + rst);
+        return rst;
     }
-    
+
     static void verifyBind(String bindInfo) {
         try {
             String encryped = bindInfo.substring(0, 236);
@@ -43,125 +45,134 @@ public class Client {
             String decrypted = AESUtil.decrypt(encryped);
             String pt = decrypted.substring(0, 128);
             String kABstr = decrypted.substring(128);
-            
+
             // 还没有验证K
 
 
 //            System.out.println("token: " + token);
 //            System.out.println("pt: " + pt);
             storeNamePt(token, pt);
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     static void authorize(String M, Token token) {
         System.out.println("\nAuthorize start!");
         String name = token.name;
+        System.out.println("Search pt of token: " + token);
         String pt = getPt(name);
-        String t = generateT(M, pt);
-        boolean b = token.validate(t + M);
-        
-        if (b) {
-            System.out.println("Allowed!");
+        if (pt.length() == 0) {
+            System.out.println("Not trusted client!");
         } else {
-            System.out.println("Rejected!");
+            System.out.println("client found pt: " + pt);
+            String t = generateT(M, pt);
+            boolean b = token.validate(t + M);
+
+            if (b) {
+                System.out.println("Allowed!");
+            } else {
+                System.out.println("Rejected!");
+            }
         }
     }
-    
+
     private static String generateT(String M, String pt) {
         String t = pt + M;
         return t;
     }
-    
+
     private static String generatePinH(String pin) {
         String pinH = pin;
         return pinH;
     }
-    
+
     private static String generateRst(String pin, String pkAStr) {
         String key = generateKey(pkAStr);
         String pkBstr = key.substring(0, 124);
+        System.out.println("pkBstr: " + pkBstr);
         String kBAstr = key.substring(124);
-        
+        System.out.println("kBAstr: " + kBAstr);
+
         String src = pin + kBAstr;
         String encrypted = AESUtil.encrypt(src);
+        System.out.println("encrypted: " + encrypted);
         String rst = pkBstr + encrypted;
         return rst;
     }
-    
+
     private static String generateKey(String pkAStr) {
         try {
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("EC");
             ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256r1");
             keyGen.initialize(ecSpec);
-            
+
             KeyPair keyPairB = keyGen.generateKeyPair();
             KeyAgreement kaB = KeyAgreement.getInstance("ECDH");
             kaB.init(keyPairB.getPrivate());
-            
+
             // 公钥
             PublicKey publicKeyB = keyPairB.getPublic();
             String pkBstr = Base64.encodeBytes(publicKeyB.getEncoded());    // 124 bits
-            
+
             // 对称密钥
             byte[] pkAbyte = Base64.decode(pkAStr);
             KeyFactory kf = KeyFactory.getInstance("EC");
             X509EncodedKeySpec pkSpec = new X509EncodedKeySpec(pkAbyte);
             PublicKey pkA = kf.generatePublic(pkSpec);
-            
+
             kaB.doPhase(pkA, true);
             byte[] kBA = kaB.generateSecret();
             String kBAstr = Base64.encodeBytes(kBA);
-            
+
             return pkBstr + kBAstr;
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-    
+
     private static String generatePin(int len) {
         // 还没有去重操作（）
         // ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
         final String chars = "0123456789";
-        
+
         SecureRandom random = new SecureRandom();
         StringBuilder sb = new StringBuilder();
-        
+
         for (int i = 0; i < len; i++) {
             int randomIndex = random.nextInt(chars.length());
             sb.append(chars.charAt(randomIndex));
         }
         return sb.toString();
     }
-    
+
     private static void storeNamePt(String token, String pt) {
         DatabaseOp dbOp = new DatabaseOp();
         dbOp.getConnection();
-        
+
         String statement = "insert into client_lib (token, pt)";
         String[] para = {token, pt};
         dbOp.insert(statement, para);
-        
+
         dbOp.closeConnection();
-        
+
         System.out.println("Client bound.");
     }
-    
+
     private static String getPt(String token) {
         DatabaseOp dbOp = new DatabaseOp();
         dbOp.getConnection();
-        
+
         String sql = "select pt from client_lib where token = '" + token + "'";
         String pt = dbOp.select(sql);
-        
+
         dbOp.closeConnection();
         return pt;
     }
-    
+
     public void register(Token token, String intendedIds, Server server) {
         String ch = server.rChallenge();
         String uid = ch.substring(0, 512);
@@ -185,7 +196,7 @@ public class Client {
             }
         }
     }
-    
+
     public void login(Token token, String intendedIds, Server server) {
         String ch = server.aChallenge();
         String r = ch.substring(0, 128);
@@ -197,13 +208,13 @@ public class Client {
             String hr = Utils.SHA256(r);
             sb.append(hr);
             sb.append(ids);
-            
+
             String res = token.aResponse(ids, hr);
             String ans = server.aCheck(res);
             if (ans.equals("FAILED")) {
                 System.out.println("Log-in Failed");
             } else {
-                System.out.println("NOW Log-in: "+ans);
+                System.out.println("NOW Log-in: " + ans);
             }
         }
     }
--- src/Token.java
+++ src/Token.java
@@ -1,5 +1,3 @@
-import jdk.jshell.execution.Util;
-
 import javax.crypto.KeyAgreement;
 import java.io.IOException;
 import java.security.*;
@@ -14,11 +12,12 @@ public class Token {
     private int n = 8;
     private int m = 3;
     public int tokenVersion;
+
     public Token(String name, int version) {
         this.name = name;
         tokenVersion = version;
     }
-    
+
     boolean setup() {
         System.out.println("\nSetup start!");
         try {
@@ -26,15 +25,16 @@ public class Token {
             KeyAgreement kaA = KeyAgreement.getInstance("ECDH");
             kaA.init(keyPairA.getPrivate());
             PublicKey publicKeyA = keyPairA.getPublic();
-            String pbkAstr = Base64.encodeBytes(publicKeyA.getEncoded());
-            
+            String pkAstr = Base64.encodeBytes(publicKeyA.getEncoded());
+            System.out.println("pkAstr: " + pkAstr);
+
             // 接收client的公钥、pin、协同密钥
             // 验证ECDH，若验证通过则返回 (K + client生成的pin)，否则返回null
-            String rst = Client.requestSetup(pbkAstr);
+            String rst = Client.requestSetup(pkAstr);
             String KPin = verify(kaA, rst, 6);
-            String pin = KPin.substring(44);
-            
-            if (pin != null) {
+
+            if (KPin != null) {
+                String pin = KPin.substring(44);
                 System.out.println("Token verified ECDH in setup.");
                 storePin(pin);
                 return true;
@@ -42,13 +42,13 @@ public class Token {
                 System.out.println("Risk of tampering, terminated.");
                 return false;
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
     }
-    
+
     boolean bind() {
         System.out.println("\nBind start!");
         try {
@@ -57,25 +57,25 @@ public class Token {
             kaA.init(keyPairA.getPrivate());
             PublicKey publicKeyA = keyPairA.getPublic();
             String pbkAstr = Base64.encodeBytes(publicKeyA.getEncoded());
-            
+
             // 接收client的公钥、pinH、协同密钥
             // 验证ECDH，若验证通过则返回(K + client计算的pinH)，否则返回null
             String rst = Client.requestBind(pbkAstr);
             String KPinH = verify(kaA, rst, 6);
-            String pinH = KPinH.substring(44);
-            
-            if (pinH != null) {
+
+            if (KPinH != null) {
+                String pinH = KPinH.substring(44);
                 System.out.println("Token verified ECDH in bind.");
                 if (pinH.equals(this.pinH)) {
                     System.out.println("Token bound.");
-                    
+
                     // 把绑定信息返回给client
                     String pt = generatePt();    // 128 bits
                     String kABstr = KPinH.substring(0, 44);
                     String decrypted = AESUtil.encrypt(pt + kABstr);    // 236 bits
                     String bindInfo = decrypted + name;
                     Client.verifyBind(bindInfo);
-                    
+
                     return true;
                 } else {
                     System.out.println("Wrong PIN!");
@@ -86,25 +86,25 @@ public class Token {
                 System.out.println("Risk of tampering, terminated.");
                 return false;
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
     }
-    
+
     boolean validate(String message) {
         String t = message.substring(0, 128);    // len取决于HMAC
         String M = message.substring(128);
         String tCal = generateT(M, pt);
-        
+
         if (t.equals(tCal)) {
             return true;
         } else {
             return false;
         }
     }
-    
+
     private String verify(KeyAgreement kaA, String rst, int len) {
         try {
             String pkBstr = rst.substring(0, 124);
@@ -112,82 +112,85 @@ public class Token {
             String decrypted = AESUtil.decrypt(encrypted);
             String pin = decrypted.substring(0, len);    // pin or pinH
             String kBAstr = decrypted.substring(len);
-            
+            System.out.println("kBAstr: " + kBAstr);
+
             byte[] pkBbyte = Base64.decode(pkBstr);
             KeyFactory kf = KeyFactory.getInstance("EC");
             X509EncodedKeySpec pkSpec = new X509EncodedKeySpec(pkBbyte);
             PublicKey pkB = kf.generatePublic(pkSpec);
-            
+
             kaA.doPhase(pkB, true);
             byte[] kAB = kaA.generateSecret();
             String kABstr = Base64.encodeBytes(kAB);    // 44 bits
-            
+            System.out.println("kABstr: " + kABstr);
+
             String K = generateK(kABstr);
-            
+
             if (kABstr.equals(kBAstr)) {
                 return K + pin;
             } else {
                 return null;
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-    
+
     private String generateT(String M, String pt) {
         String t = pt + M;
         return t;
     }
-    
+
     private String generatePt() {
         // 还没有去重操作（）
         final String chars = "01";
-        
+
         SecureRandom random = new SecureRandom();
         StringBuilder sb = new StringBuilder();
-        
+
         for (int i = 0; i < 128; i++) {
             int randomIndex = random.nextInt(chars.length());
             sb.append(chars.charAt(randomIndex));
         }
-        
+
         String pt = sb.toString();
         this.pt = pt;
         return pt;
     }
-    
+
     private String generateK(String kABstr) {
         String K = kABstr;
         return K;
     }
-    
+
     private KeyPair generateKeyPair() {
         try {
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("EC");
             ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256r1");
             keyGen.initialize(ecSpec);
-            
+
             KeyPair keyPairA = keyGen.generateKeyPair();
             return keyPairA;
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-    
+
     private boolean storePin(String pin) {
         // 根据pin生成pinH
         String pinH = pin;
-        
-        
+
+
         this.pinH = pinH;
         System.out.println("Token finished setup.");
         System.out.println("PIN: " + pin);
         return true;
     }
-    
+
     public Object[] rResponse(String ids, String uid, String hr) {
         // if (not setup) {setup}
         // if (not bound) {bound}
@@ -214,20 +217,20 @@ public class Token {
         // store the information into database
         DatabaseOp db = new DatabaseOp();
         db.getConnection();
-        db.insertTokenCredential(name,ids,cid,n,sk,pk);
+        db.insertTokenCredential(name, ids, cid, n, sk, pk);
         db.closeConnection();
         Object[] ans = new Object[2];
         ans[0] = ad;
         ans[1] = sig;
         return ans;
     }
-    
+
     public String aResponse(String ids, String hr) {
         // select from database according to ids
         DatabaseOp db = new DatabaseOp();
         db.getConnection();
-        String cid = db.selectCid(name,ids);
-        int n = db.selectN(name,ids);
+        String cid = db.selectCid(name, ids);
+        int n = db.selectN(name, ids);
         n++;
         //ad
         StringBuilder ad = new StringBuilder();
@@ -235,17 +238,17 @@ public class Token {
         ad.append(Utils.intToBinStr(n));
         ad.append(cid);
         //sign
-        
+
         // update credential info
-        db.updateN_token(name,ids,n);
+        db.updateN_token(name, ids, n);
         db.closeConnection();
         //send
         return ad.toString();
     }
-    
-    public static String[] keyGen(){
+
+    public static String[] keyGen() {
         String[] ans = new String[2];
-        try{
+        try {
             KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
 //            RSAKeyGenParameterSpec spec = new RSAKeyGenParameterSpec(2048,RSAKeyGenParameterSpec.F4);
             keyPairGen.initialize(2048);
@@ -254,7 +257,7 @@ public class Token {
             PrivateKey privateKey = keyPair.getPrivate();
             ans[0] = Base64.encodeBytes(publicKey.getEncoded());
             ans[1] = Base64.encodeBytes(privateKey.getEncoded());
-        }catch (NoSuchAlgorithmException e){
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return ans;
